4325fa61f0eb4ea9f8d5e752b0fea872e4949fb8
Chidiebere Onyedinma <codebreakersmd@gmail.com>
2025-05-04 16:12:56 -0400

    Added Top bar for custom AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,3 +22,20 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+class TopBar extends StatefulWidget implements PreferredSizeWidget {
+  const TopBar({super.key});
+
+  @override
+  State<TopBar> createState() => _TopBarState();
+
+  @override
+  Size get preferredSize => Size(double.maxFinite, 80);
+}
+
+class _TopBarState extends State<TopBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
